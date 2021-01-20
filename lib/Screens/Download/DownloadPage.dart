a62d042424af5eeeedba39f9c31be52a90916f70
vuhoang <vhtrinh73@gmail.com>
2021-01-20 18:01:40 +0700

    Update

--- lib/Screens/Download/DownloadPage.dart
+++ lib/Screens/Download/DownloadPage.dart
@@ -1,9 +1,11 @@
+import 'package:downloads_path_provider/downloads_path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-import 'package:DARKEN/Screens/Home/CourseDetailPage.dart';
-
 import 'package:DARKEN/Styling/AppColors.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
 
 class DownloadPage extends StatefulWidget {
   static String tag = '/download-page';
@@ -13,6 +15,29 @@ class DownloadPage extends StatefulWidget {
 }
 
 class _DownloadPage extends State<DownloadPage> {
+
+  String appDirectory;
+  List<FileSystemEntity> listVideoFiles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _listofFiles();
+
+  }
+
+  void _listofFiles() async {
+    final dir = await getApplicationDocumentsDirectory();
+    appDirectory = dir.path;
+    setState(() {
+      dir.list(recursive: false, followLinks: false)
+          .listen((FileSystemEntity entity) {
+            if (entity.path.substring(dir.path.length+1, entity.path.length) != ".Trash") {
+              listVideoFiles.add(entity);
+            }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,15 +63,44 @@ class _DownloadPage extends State<DownloadPage> {
                     ),
                   ),
                   Expanded(
-                    child: Container(
+                    child: listVideoFiles == null || listVideoFiles.length == 0 ? Container(
                       alignment: Alignment.center,
                       child: Text(
-                        'There are no downloaded courses!',
+                        'There are no downloaded videos!',
                         style: TextStyle(
                           fontSize: 18,
                           color: Colors.grey,
                         ),
                       ),
+                    ): Container(
+                        height: listVideoFiles.length.toDouble()*60,
+                        child: ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: listVideoFiles.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                child : GestureDetector(
+                                    onTap: () {
+                                      OpenFile.open(listVideoFiles[index].path);
+                                    },
+                                    child: Card(
+                                      child: Container(
+                                          padding: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Text(
+                                                  listVideoFiles[index].path.substring(appDirectory.length + 1, listVideoFiles[index].path.length),
+                                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                                            ],
+                                          )
+                                      ),
+                                    )
+                                )
+                            );
+                          },
+                        )
                     ),
                   ),
                 ],
@@ -56,3 +110,5 @@ class _DownloadPage extends State<DownloadPage> {
     );
   }
 }
+
+// print();
--- lib/Screens/Home/VideoPlayerWithExcercisesPage.dart
+++ lib/Screens/Home/VideoPlayerWithExcercisesPage.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'package:DARKEN/APIs/APIServer.dart';
 import 'package:DARKEN/Models/CourseWithLessonModel.dart';
 import 'package:DARKEN/Models/ExerciseModel.dart';
 import 'package:DARKEN/Models/LessonVideoModel.dart';
 import 'package:DARKEN/Styling/AppColors.dart';
+import 'package:dio/dio.dart';
+import 'package:downloads_path_provider/downloads_path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:video_player/video_player.dart';
+import 'package:path/path.dart' as path;
 
 class VideoPlayerWithExercisesPage extends StatefulWidget {
+  static String tag = '/video-player-with-exercise-page';
   String courseID;
   Lesson lesson;
   VideoPlayerWithExercisesPage({Key key, this.courseID, this.lesson}): super(key: key);
@@ -16,13 +26,38 @@ class VideoPlayerWithExercisesPage extends StatefulWidget {
 }
 
 class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> {
+  final Dio _dio = Dio();
   VideoPlayerController _controller;
   Future<void> _initializeVideoPlayerFuture;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
+  String _progressDownload = "";
   bool _isLoading = false;
+  bool _isDownloading = false;
 
   List<ExerciseModel> listExercises;
   LessonVideoModel lessonVideo;
+
+  @override
+  void initState() {
+
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    final android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    final iOS = IOSInitializationSettings();
+    final initSettings = InitializationSettings(android, iOS);
+
+    flutterLocalNotificationsPlugin.initialize(initSettings, onSelectNotification: _onSelectNotification);
+
+    _fetchData();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   void _fetchData() async {
     setState(() {
@@ -38,18 +73,114 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
     });
   }
 
-  @override
-  void initState() {
-
-    _fetchData();
-
-    super.initState();
+  Future<Directory> _getDownloadDirectory() async {
+    if (Platform.isAndroid) {
+      return await DownloadsPathProvider.downloadsDirectory;
+    }
+    return await getApplicationDocumentsDirectory();
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  Future<bool> _requestPermissions() async {
+    var permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+
+    if (permission != PermissionStatus.granted) {
+      await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    }
+
+    return permission == PermissionStatus.granted;
+  }
+
+
+  Future<void> _download() async {
+    final dir = await _getDownloadDirectory();
+    final isPermissionStatusGranted = await _requestPermissions();
+
+    if (isPermissionStatusGranted) {
+      setState(() {
+        _isDownloading = true;
+      });
+      final savePath = path.join(dir.path, widget.lesson.name + ".mp4");
+      await _startDownload(savePath);
+      setState(() {
+        _isDownloading = false;
+      });
+    }
+    else {
+      print("User declined permission!");
+    }
+  }
+
+
+  Future<void> _startDownload(String savePath) async {
+    Map<String, dynamic> result = {
+      'isSuccess': false,
+      'filePath': null,
+      'error': null,
+    };
+
+    try {
+      final response = await _dio.download(
+          lessonVideo.videoUrl,
+          savePath,
+          onReceiveProgress: _onReceiveProgress
+      );
+      result['isSuccess'] = response.statusCode == 200;
+      result['filePath'] = savePath;
+    }
+    catch (ex) {
+      result['error'] = ex.toString();
+    }
+    finally {
+      await _showNotification(result);
+    }
+  }
+
+  void _onReceiveProgress(int received, int total) {
+    if (total != -1) {
+      setState(() {
+        _progressDownload = (received / total * 100).toStringAsFixed(0) + "%";
+      });
+    }
+  }
+
+  Future<void> _onSelectNotification(String json) async {
+    final obj = jsonDecode(json);
+
+    if (obj['isSuccess']) {
+      OpenFile.open(obj['filePath']);
+    }
+    else {
+      showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+          title: Text('Error'),
+          content: Text('${obj['error']}'),
+        ),
+      );
+    }
+  }
+
+  Future<void> _showNotification(Map<String, dynamic> downloadStatus) async {
+    final android = AndroidNotificationDetails(
+        'channel id',
+        'channel name',
+        'channel description',
+        priority: Priority.High,
+        importance: Importance.Max
+    );
+    final iOS = IOSNotificationDetails();
+    final platform = NotificationDetails(android, iOS);
+    final json = jsonEncode(downloadStatus);
+    final isSuccess = downloadStatus['isSuccess'];
+
+    await flutterLocalNotificationsPlugin.show(
+        0, // notification id
+        isSuccess ? 'Success' : 'Failure',
+        isSuccess ? 'Your lesson video has been downloaded successfully!' : 'There was an error while downloading the file.',
+        platform,
+        payload: json
+    );
   }
 
   @override
@@ -72,6 +203,47 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
                 child: new Center(
                     child: new CircularProgressIndicator()
                 )
+            ),
+          ),
+          alignment: FractionalOffset.center,
+        )
+      ],
+    ) : new Container();
+
+    Widget downloadingIndicator = _isDownloading ? new Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Color.fromRGBO(0, 0, 0, 0.2),
+        ),
+        Align(
+          child: Container(
+            color: Colors.grey[700],
+            width: 100,
+            height: 100,
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: new Center(
+                        child: new CircularProgressIndicator()
+                    )
+                ),
+                Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        _progressDownload,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: AppColors.darkGreyColor,
+                        ),
+                      ),
+                    )
+                )
+              ],
             ),
           ),
           alignment: FractionalOffset.center,
@@ -118,6 +290,16 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
                       ),
                     ),
                     Container(
+                      padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                      height: 50,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+                        onPressed: _download,
+                        color: AppColors.themeColor,
+                        child: Text('Download Video', style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                    Container(
                       padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                       child: Text(
                         "DESCRIPTION",
@@ -132,6 +314,7 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
                       padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
                       child: Text("Hours: " + lessonVideo.currentTime.toString()),
                     ),
+
                     Container(
                       padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                       child: Text(
@@ -183,6 +366,10 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
           Align(
               child: loadingIndicator,
               alignment: FractionalOffset.center
+          ),
+          Align(
+              child: downloadingIndicator,
+              alignment: FractionalOffset.center
           )
         ],
       ),
@@ -205,4 +392,3 @@ class _VideoPlayerWithExercisesPage extends State<VideoPlayerWithExercisesPage> 
     );
   }
 }
-
